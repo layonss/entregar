83992844285509d6a21e1ca71b6f0008964ba091
layonss <112710373+layonss@users.noreply.github.com>
2022-09-28 15:36:13 -0300

    Update animal01.ts

--- animal01.ts
+++ animal01.ts
@@ -1,2 +1,15 @@
-import { animal } from "./class/animal";
-let a1 = new cavalo ()
+import { Cachorro } from "./SRC/cachorro";
+import { Cavalo } from "./SRC/cavalo";
+import { Preguica } from "./SRC/preguiça";
+
+
+let cachorro1 = new Cachorro ("Bartolomeu", 6);
+let cavalo1 = new Cavalo ("Pocotó", 15);
+let preguiça1 = new Preguica ("Pepé", 12);
+
+cachorro1.Correr();
+cachorro1.EmitirSom();
+cavalo1.Correr();
+cavalo1.EmitirSom();
+preguiça1.EmitirSom();
+preguiça1.SubiremArvores();
